176af3b088eefd611906ac014fa4299ddb0f1cc3
Domenico Pranteda <dpranteda@3di.it>
2016-05-23 17:32:54 +0200

    PersonaWrapper work in progress

---
+++ src/main/java/it/tredi/ecm/web/bean/PersonaWrapper.java
@@ -0,0 +1,26 @@
+package it.tredi.ecm.web.bean;
+
+import it.tredi.ecm.dao.entity.File;
+import it.tredi.ecm.dao.entity.Persona;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonaWrapper extends Wrapper {
+	private Persona persona;
+	private Long accreditamentoId;
+	private Long providerId;
+	
+	public void setOffsetAndIds(){
+		if(persona.isLegaleRappresentante())
+			setIdOffset(17);
+		else if(persona.isLegaleRappresentante())
+			setIdOffset(25);
+	}
+	
+	
+	private File nomina;
+	private File curriculumVitae;
+	private File delega;
+}
