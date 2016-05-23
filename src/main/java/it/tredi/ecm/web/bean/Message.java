cfb96c2d4f9475255f170cfced8833dcb9ca6b81
tiommi <tiommi@trasferte-lap1>
2016-05-23 15:53:08 +0200

    Bean per la gestione dei pNotify.

---
+++ src/main/java/it/tredi/ecm/web/bean/Message.java
@@ -0,0 +1,12 @@
+package it.tredi.ecm.web.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+	private String titolo;
+	private String testo;
+	private String tipo;
+}
