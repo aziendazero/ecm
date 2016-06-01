c2b7e71b189b345045e4941502cd2fa4505f11d2
tiommi <tiommi@trasferte-lap1>
2016-06-01 16:58:43 +0200

    Professioni/discipline di prova

--- src/main/java/it/tredi/ecm/bootstrap/ProfessioniDisciplineLoader.java
+++ src/main/java/it/tredi/ecm/bootstrap/ProfessioniDisciplineLoader.java
@@ -40,14 +40,46 @@ public class ProfessioniDisciplineLoader implements ApplicationListener<ContextR
 			discipline.add(d1);
 			Disciplina d2 = new Disciplina("d2");
 			discipline.add(d2);
-			//.....
+			Disciplina d3 = new Disciplina("d3");
+			discipline.add(d3);
+			Disciplina d4 = new Disciplina("d4");
+			discipline.add(d4);
+			Disciplina d5 = new Disciplina("d5");
+			discipline.add(d5);
+			Disciplina d6 = new Disciplina("d6");
+			discipline.add(d6);
+			Disciplina d7 = new Disciplina("d7");
+			discipline.add(d7);
+			Disciplina d8 = new Disciplina("d8");
+			discipline.add(d8);
+			Disciplina d9 = new Disciplina("d9");
+			discipline.add(d9);
+			Disciplina d10 = new Disciplina("d10");
+			discipline.add(d10);
 			
 			disciplinaService.saveAll(discipline);
 			
-			Professione p1 = new Professione();
+			Professione p1 = new Professione("p1");
 			p1.addDisciplina(d1);
 			p1.addDisciplina(d2);
+			p1.addDisciplina(d3);
+			p1.addDisciplina(d4);
+			Professione p2 = new Professione("p2");
+			p2.addDisciplina(d5);
+			p2.addDisciplina(d6);
+			Professione p3 = new Professione("p3");
+			p3.addDisciplina(d7);
+			Professione p4 = new Professione("p4");
+			p4.addDisciplina(d8);
+			Professione p5 = new Professione("p5");
+			p5.addDisciplina(d9);
+			p5.addDisciplina(d10);
+
 			professioneService.save(p1);
+			professioneService.save(p2);
+			professioneService.save(p3);
+			professioneService.save(p4);
+			professioneService.save(p5);
 		}
 	}
 }
