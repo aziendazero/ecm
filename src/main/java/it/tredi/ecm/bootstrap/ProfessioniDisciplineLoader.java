59f5a9db6a090f914fb796cc8139c772ae529fdf
Domenico Pranteda <dpranteda@3di.it>
2016-07-01 15:32:15 +0200

    Corretto il meccanismo di popolamento della lista professioni/discipline che prima salvava solo una disciplina

--- src/main/java/it/tredi/ecm/bootstrap/ProfessioniDisciplineLoader.java
+++ src/main/java/it/tredi/ecm/bootstrap/ProfessioniDisciplineLoader.java
@@ -32,242 +32,311 @@ public class ProfessioniDisciplineLoader implements ApplicationListener<ContextR
 		LOGGER.info("Bootsrap ECM - Initializing PROFESSIONI / DISCIPLINE...");
 		
 		//Set<Professione> professioni = professioneService.getAllProfessioni();
-		Set<Disciplina> discipline = disciplinaService.getAllDiscipline();
+		Set<Disciplina> disciplinea = disciplinaService.getAllDiscipline();
 			
-		if(discipline.isEmpty()){
+		if(disciplinea.isEmpty()){
 			Disciplina d1 = new Disciplina("Allergologia ed immunologia clinica");
-			discipline.add(d1);
+			disciplinaService.save(d1);
+			
 			Disciplina d2 = new Disciplina("Angiologia");
-			discipline.add(d2);
+			disciplinaService.save(d2);
+			
 			Disciplina d3 = new Disciplina("Cardiologia");   
-			discipline.add(d3);
+			disciplinaService.save(d3);
+			
 			Disciplina d4 = new Disciplina("Dermatologia e venereologia");   
-			discipline.add(d4);
+			disciplinaService.save(d4);
+			
 			Disciplina d5 = new Disciplina("Ematologia");   
-			discipline.add(d5);
+			disciplinaService.save(d5);
+			
 			Disciplina d6 = new Disciplina("Endocrinologia");   
-			discipline.add(d6);
+			disciplinaService.save(d6);
+			
 			Disciplina d7 = new Disciplina("Gastroenterologia");   
-			discipline.add(d7);
+			disciplinaService.save(d7);
+			
 			Disciplina d8 = new Disciplina("Genetica medica");   
-			discipline.add(d8);
+			disciplinaService.save(d8);
+			
 			Disciplina d9 = new Disciplina("Geriatria");   
-			discipline.add(d9);
+			disciplinaService.save(d9);
+			
 			Disciplina d10 = new Disciplina("Malattie metaboliche e diabetologia");   
-			discipline.add(d10);
+			disciplinaService.save(d10);
+			
 			Disciplina d11 = new Disciplina("Malattie dell'apparato respiratorio");   
-			discipline.add(d11);
+			disciplinaService.save(d11);
+			
 			Disciplina d12 = new Disciplina("Malattie infettive");   
-			discipline.add(d12);
+			disciplinaService.save(d12);
+			
 			Disciplina d13 = new Disciplina("Medicina e chirurgia di accettazione e di urgenza");   
-			discipline.add(d13);
+			disciplinaService.save(d13);
+			
 			Disciplina d14 = new Disciplina("Medicina fisica e riabilitazione");   
-			discipline.add(d14);
+			disciplinaService.save(d14);
+			
 			Disciplina d15 = new Disciplina("Medicina interna");   
-			discipline.add(d15);
+			disciplinaService.save(d15);
+			
 			Disciplina d16 = new Disciplina("Medicina termale");   
-			discipline.add(d16);
+			disciplinaService.save(d16);
+			
 			Disciplina d17 = new Disciplina("Medicina aeronautica e spaziale");   
-			discipline.add(d17);
+			disciplinaService.save(d17);
+			
 			Disciplina d18 = new Disciplina("Medicina dello sport");   
-			discipline.add(d18);
+			disciplinaService.save(d18);
+			
 			Disciplina d19 = new Disciplina("Nefrologia");   
-			discipline.add(d19);
+			disciplinaService.save(d19);
+			
 			Disciplina d20 = new Disciplina("Neonatologia");   
-			discipline.add(d20);
+			disciplinaService.save(d20);
+			
 			Disciplina d21 = new Disciplina("Neurologia");   
-			discipline.add(d21);
+			disciplinaService.save(d21);
+			
 			Disciplina d22 = new Disciplina("Neuropsichiatria infantile");   
-			discipline.add(d22);
+			disciplinaService.save(d22);
+			
 			Disciplina d23 = new Disciplina("Oncologia");   
-			discipline.add(d23);
+			disciplinaService.save(d23);
+			
 			Disciplina d24 = new Disciplina("Pediatria");   
-			discipline.add(d24);
+			disciplinaService.save(d24);
+			
 			Disciplina d25 = new Disciplina("Psichiatria");   
-			discipline.add(d25);
+			disciplinaService.save(d25);
+			
 			Disciplina d26 = new Disciplina("Radioterapia");   
-			discipline.add(d26);
+			disciplinaService.save(d26);
+			
 			Disciplina d27 = new Disciplina("Reumatologia");   
-			discipline.add(d27);
+			disciplinaService.save(d27);
+			
 			Disciplina d29 = new Disciplina("Cardiochirurgia");   
-			discipline.add(d29);
+			disciplinaService.save(d29);
+			
 			Disciplina d30 = new Disciplina("Chirurgia generale");   
-			discipline.add(d30);
+			disciplinaService.save(d30);
+			
 			Disciplina d31 = new Disciplina("Chirurgia maxillo-facciale");   
-			discipline.add(d31);
+			disciplinaService.save(d31);
+			
 			Disciplina d32 = new Disciplina("Chirurgia pediatrica");   
-			discipline.add(d32);
+			disciplinaService.save(d32);
+			
 			Disciplina d33 = new Disciplina("Chirurgia plastica e ricostruttiva");   
-			discipline.add(d33);
+			disciplinaService.save(d33);
+			
 			Disciplina d34 = new Disciplina("Chirurgia toracica");   
-			discipline.add(d34);
+			disciplinaService.save(d34);
+			
 			Disciplina d35 = new Disciplina("Chirurgia vascolare");   
-			discipline.add(d35);
+			disciplinaService.save(d35);
+			
 			Disciplina d36 = new Disciplina("Ginecologia e ostetricia");   
-			discipline.add(d36);
+			disciplinaService.save(d36);
+			
 			Disciplina d37 = new Disciplina("Neurochirurgia");   
-			discipline.add(d37);
+			disciplinaService.save(d37);
+			
 			Disciplina d38 = new Disciplina("Oftalmologia");   
-			discipline.add(d38);
+			disciplinaService.save(d38);
+			
 			Disciplina d39 = new Disciplina("Ortopedia e traumatologia");  
-			discipline.add(d39);
+			disciplinaService.save(d39);
+			
 			Disciplina d40 = new Disciplina("Otorinolaringoiatria");   
-			discipline.add(d40);
+			disciplinaService.save(d40);
+			
 			Disciplina d41 = new Disciplina("Urologia");  
-			discipline.add(d41);
+			disciplinaService.save(d41);
+			
 			Disciplina d42 = new Disciplina("Anatomia patologica");  
-			discipline.add(d42);
+			disciplinaService.save(d42);
+			
 			Disciplina d43 = new Disciplina("Anestesia e rianimazione"); 
-			discipline.add(d43);
+			disciplinaService.save(d43);
+			
 			Disciplina d44 = new Disciplina("Biochimica clinica");  
-			discipline.add(d44);
+			disciplinaService.save(d44);
+			
 			Disciplina d45 = new Disciplina("Farmacologia e tossicologia clinica");   
-			discipline.add(d45);
+			disciplinaService.save(d45);
+			
 			Disciplina d46 = new Disciplina("Laboratorio di genetica medica");
-			discipline.add(d46);
+			disciplinaService.save(d46);
+			
 			Disciplina d47 = new Disciplina("Medicina trasfusionale"); 
-			discipline.add(d47);
+			disciplinaService.save(d47);
+			
 			Disciplina d48 = new Disciplina("Medicina legale"); 
-			discipline.add(d48);
+			disciplinaService.save(d48);
+			
 			Disciplina d49 = new Disciplina("Medicina nucleare"); 
-			discipline.add(d49);
+			disciplinaService.save(d49);
+			
 			Disciplina d50 = new Disciplina("Microbiologia e virologia");  
-			discipline.add(d50);
+			disciplinaService.save(d50);
+			
 			Disciplina d51 = new Disciplina("Neurofisiopatologia");  
-			discipline.add(d51);
+			disciplinaService.save(d51);
+			
 			Disciplina d52 = new Disciplina("Neuroradiologia");
-			discipline.add(d52); 
+			disciplinaService.save(d52);
+			
 			Disciplina d53 = new Disciplina("Patologia clinica (laboratorio di analisi chimico-cliniche e microbiologia)");   
-			discipline.add(d53);
+			disciplinaService.save(d53);
+			
 			Disciplina d54 = new Disciplina("Radiodiagnostica");  
-			discipline.add(d54);
+			disciplinaService.save(d54);
+			
 			Disciplina d55 = new Disciplina("Igiene, epidemiologia e sanità pubblica");  
-			discipline.add(d55);
+			disciplinaService.save(d55);
+			
 			Disciplina d56 = new Disciplina("Igiene degli alimenti e della nutrizione");  
-			discipline.add(d56);
+			disciplinaService.save(d56);
+			
 			Disciplina d57 = new Disciplina("Medicina del lavoro e sicurezza degli ambienti di lavoro");  
-			discipline.add(d57);
+			disciplinaService.save(d57);
+			
 			Disciplina d58 = new Disciplina("Medicina generale (medici di famiglia)");  
-			discipline.add(d58);
+			disciplinaService.save(d58);
+			
 			Disciplina d59 = new Disciplina("Continuità assistenziale");  
-			discipline.add(d59);
+			disciplinaService.save(d59);
+			
 			Disciplina d60 = new Disciplina("Pediatria (pediatri di libera scelta)");  
-			discipline.add(d60);
+			disciplinaService.save(d60);
+			
 			Disciplina d106 = new Disciplina("Scienza dell'alimentazione e dietetica");  
-			discipline.add(d106);
+			disciplinaService.save(d106);
+			
 			Disciplina d107 = new Disciplina("Direzione medica di presidio ospedaliero");  
-			discipline.add(d107);
+			disciplinaService.save(d107);
+			
 			Disciplina d108 = new Disciplina("Organizzazione dei servizi sanitari di	base"); 
-			discipline.add(d108);
+			disciplinaService.save(d108);
+			
 			Disciplina d111 = new Disciplina("Audiologia e foniatria");  
-			discipline.add(d111);
+			disciplinaService.save(d111);
+			
 			Disciplina d112 = new Disciplina("Psicoterapia"); 
-			discipline.add(d112);
+			disciplinaService.save(d112);
+			
 			Disciplina d113 = new Disciplina("Privo di specializzazione");  
-			discipline.add(d113);
+			disciplinaService.save(d113);
+			
 			Disciplina d114 = new Disciplina("Cure palliative");  
-			discipline.add(d114);
+			disciplinaService.save(d114);
+			
 			Disciplina d115 = new Disciplina("Epidemiologia");  
-			discipline.add(d115);
+			disciplinaService.save(d115);
+			
 			Disciplina d116 = new Disciplina("Medicina di comunità"); 
-			discipline.add(d116);
-		
+			disciplinaService.save(d116);
+			
 			Disciplina d64 = new Disciplina("Odontoiatria");  
-			discipline.add(d64);
-		
+			disciplinaService.save(d64);
+			
 			Disciplina d66 = new Disciplina("Farmacia ospedaliera");  
-			discipline.add(d66);
+			disciplinaService.save(d66);
+			
 			Disciplina d67 = new Disciplina("Farmacia territoriale");  
-			discipline.add(d67);
-		
+			disciplinaService.save(d67);
+			
 			Disciplina d61 = new Disciplina("Igiene degli allevamenti e delle produzioni zootecniche"); 
-			discipline.add(d61);
+			disciplinaService.save(d61);
+			
 			Disciplina d62 = new Disciplina("Igiene prod., trasf., commercial., conserv. E tras. Alimenti di origine animale e derivati");  
-			discipline.add(d62);
+			disciplinaService.save(d62);
+			
 			Disciplina d63 = new Disciplina("Sanità animale"); 
-			discipline.add(d63);
-		
+			disciplinaService.save(d63);
+			
 			Disciplina d77 = new Disciplina("Psicoterapia"); 
-			discipline.add(d77);
+			disciplinaService.save(d77);
+			
 			Disciplina d78 = new Disciplina("Psicologia"); 
-			discipline.add(d78);
-		
+			disciplinaService.save(d78);
+			
 			Disciplina d68 = new Disciplina("Biologo");  
-			discipline.add(d68);
-		
+			disciplinaService.save(d68);
+			
 			Disciplina d76 = new Disciplina("Chimica analitica");  
-			discipline.add(d76);
-		
+			disciplinaService.save(d76);
+			
 			Disciplina d79 = new Disciplina("Fisica sanitaria"); 
-			discipline.add(d79);
-		
+			disciplinaService.save(d79);
+			
 			Disciplina d80 = new Disciplina("Assistente sanitario");  
-			discipline.add(d80);
-		
+			disciplinaService.save(d80);
+			
 			Disciplina d81 = new Disciplina("Dietista");  
-			discipline.add(d81);
-		
+			disciplinaService.save(d81);
+			
 			Disciplina d83 = new Disciplina("Educatore professionale");
-			discipline.add(d83);
-		
+			disciplinaService.save(d83);
+			
 			Disciplina d82 = new Disciplina("Fisioterapista"); 
-			discipline.add(d82);
-		
+			disciplinaService.save(d82);
+			
 			Disciplina d84 = new Disciplina("Igienista dentale");
-			discipline.add(d84);
-		
+			disciplinaService.save(d84);
+			
 			Disciplina d85 = new Disciplina("Infermiere"); 
-			discipline.add(d85);
-		
+			disciplinaService.save(d85);
+	
 			Disciplina d86 = new Disciplina("Infermiere pediatrico"); 
-			discipline.add(d86);
-		
+			disciplinaService.save(d86);
+			
 			Disciplina d87 = new Disciplina("Logopedista");  
-			discipline.add(d87);
-		
+			disciplinaService.save(d87);
+			
 			Disciplina d88 = new Disciplina("Ortottista/Assistente di oftalmologia");  
-			discipline.add(d88);
-		
+			disciplinaService.save(d88);
+			
 			Disciplina d89 = new Disciplina("Ostetrica/o"); 
-			discipline.add(d89);
-		
+			disciplinaService.save(d89);
+			
 			Disciplina d90 = new Disciplina("Podologo");  
-			discipline.add(d90);
-		
+			disciplinaService.save(d90);
+			
 			Disciplina d95 = new Disciplina("Tecnico audiometrista");  
-			discipline.add(d95);
-		
+			disciplinaService.save(d95);
+			
 			Disciplina d96 = new Disciplina("Tecnico audioprotesista");  
-			discipline.add(d96);
-		
+			disciplinaService.save(d96);
+			
 			Disciplina d92 = new Disciplina("Tecnico della fisiopatologia cardiocircolatoria e perfusione cardiovascolare"); 
-			discipline.add(d92);
-		
+			disciplinaService.save(d92);
+			
 			Disciplina d105 = new Disciplina("Tecnico della prevenzione nell'ambiente e nei luoghi di lavoro");  
-			discipline.add(d105);
-		
+			disciplinaService.save(d105);
+			
 			Disciplina d91 = new Disciplina("Tecnico della riabilitazione psichiatrica");   
-			discipline.add(d91);
-		
+			disciplinaService.save(d91);
+			
 			Disciplina d98 = new Disciplina("Tecnico di neurofisiopatologia");  
-			discipline.add(d98);
-		
+			disciplinaService.save(d98);
+			
 			Disciplina d99 = new Disciplina("Tecnico ortopedico");  
-			discipline.add(d99);
-		
+			disciplinaService.save(d99);
+			
 			Disciplina d94 = new Disciplina("Tecnico sanitario di radiologia medica");  
-			discipline.add(d94);
-		
+			disciplinaService.save(d94);
+			
 			Disciplina d93 = new Disciplina("Tecnico sanitario laboratorio biomedico"); 
-			discipline.add(d93);
-		
+			disciplinaService.save(d93);
+			
 			Disciplina d100 = new Disciplina("Terapista della neuro e psicomotricità dell'età evolutiva"); 
-			discipline.add(d100);
-		
+			disciplinaService.save(d100);
+			
 			Disciplina d101 = new Disciplina("Terapista occupazionale"); 
-			discipline.add(d101);
-				
-			disciplinaService.saveAll(discipline);
+			disciplinaService.save(d101);
 			
 			Professione p1 = new Professione("Medico chirurgo");
 			p1.setSanitaria(true);
