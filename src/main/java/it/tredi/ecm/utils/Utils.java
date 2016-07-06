20d72d6e851f4331f04e843f65311ba380022de0
Domenico Pranteda <dpranteda@3di.it>
2016-07-06 17:39:03 +0200

    LOG ottimizzato e WIP

--- src/main/java/it/tredi/ecm/utils/Utils.java
+++ src/main/java/it/tredi/ecm/utils/Utils.java
@@ -14,7 +14,7 @@ import it.tredi.ecm.service.bean.CurrentUser;
 
 public class Utils {
 	private final static Logger LOGGER = Logger.getLogger(Utils.class);
-	
+
 	/**
 	 * Recupero dell'utente loggato
 	 * @return utente loggato oppure <code>NULL</code> se l'utente non ha fatto login
@@ -22,17 +22,16 @@ public class Utils {
 	public static CurrentUser getAuthenticatedUser(){
 		CurrentUser currentUser = null;
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		
+
 		if(authentication instanceof AnonymousAuthenticationToken){
 			LOGGER.info("AnonymousAuthentication found");
 			return null;
 		}
-		
+
 		currentUser = (CurrentUser) authentication.getPrincipal();
 		return currentUser;
 	}
-	
-	
+
 	public static File convertFromMultiPart(MultipartFile multiPartFile){
 		try{
 			if(multiPartFile != null){
@@ -45,7 +44,22 @@ public class Utils {
 		}catch (IOException ioException){
 			LOGGER.error("Errore durante lettura contenuto del file caricato oppure nessun file passato",ioException);		
 		}
-		
+
 		return null;
+	}
+
+	public static void logInfo(org.slf4j.Logger LOGGER, String message){
+		String userInfo = "[" + getAuthenticatedUser().getAccount().getUsername() + "] - "; 
+		LOGGER.info(userInfo + message);
+	}
+
+	public static void logError(org.slf4j.Logger LOGGER, String message, Throwable ex){
+		String userInfo = "[" + getAuthenticatedUser().getAccount().getUsername() + "] - "; 
+		LOGGER.error(userInfo + message, ex);
+	}
+
+	public static void logDebug(org.slf4j.Logger LOGGER, String message){
+		String userInfo = "[" + getAuthenticatedUser().getAccount().getUsername() + "] - "; 
+		LOGGER.debug(userInfo + message);
 	}
 }
