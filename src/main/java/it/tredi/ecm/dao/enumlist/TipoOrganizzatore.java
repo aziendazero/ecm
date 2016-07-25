0e64f9e9ebda12464e25691ce02e8f063ccb8a1a
tiommi <tiommi@tiommi-desktop.bo.priv>
2016-07-25 16:05:14 +0200

    Correzione errore

--- src/main/java/it/tredi/ecm/dao/enumlist/TipoOrganizzatore.java
+++ src/main/java/it/tredi/ecm/dao/enumlist/TipoOrganizzatore.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum TipoOrganizzatore {
-	ISTITUTI_RICOVERO (14, "Istituti di ricovero e cure a a carattere sceintifico (IRCCS)", "A", true),
+	ISTITUTI_RICOVERO (14, "Istituti di ricovero e cura a carattere scientifico (IRCCS)", "A", true),
 	AZIENDE_SANITARIE (2, "Aziende Sanitarie (Aziende Usl, Aziende Ospedaliere, Policlinici)", "A", true),
 	UNIVERSITA (1, "Università, facoltà e dipartimenti universitari", "B", true),
 	ISTITUTI_SCIENTIFICI (3, "Istituti scientifici del servizio sanitario nazionale", "B", true),
@@ -20,12 +20,12 @@ public enum TipoOrganizzatore {
 	ZOOPROFILATTICO (5, "Istituto zooprofilattico", "B", true),
 	ENTE_FORMAZIONE (15, "Ente di formazione a partecipazione prevalentemente pubblica regionale o provinciale", "B", true),
 	OSPEDALI_CLASSIFICATI (16, "Ospedali classificati ex. Art. 1 legge 132 1968", "C", true);
-	
+
 	private int id;
 	private String nome;
 	private String gruppo;
 	private boolean isTipoP;
-	
+
 	private TipoOrganizzatore(int id, String nome, String gruppo, boolean isTipoP){
 		this.id = id;
 		this.nome = nome;
