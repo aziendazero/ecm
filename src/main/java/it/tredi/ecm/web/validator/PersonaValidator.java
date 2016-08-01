c63e7ea3beaa96f2adf83dda7e92da15c29158ce
tiommi <tiommi@tiommi-desktop.bo.priv>
2016-08-01 16:21:32 +0200

    Rimossa professione dal responsabile sis. informatico e qualità
    n.b. erano rimasti i controlli nel validator e i campi editabili nella
    enumlist

--- src/main/java/it/tredi/ecm/web/validator/PersonaValidator.java
+++ src/main/java/it/tredi/ecm/web/validator/PersonaValidator.java
@@ -18,42 +18,41 @@ import it.tredi.ecm.utils.Utils;
 @Component
 public class PersonaValidator {
 	private static final Logger LOGGER = LoggerFactory.getLogger(PersonaValidator.class);
-	
+
 	@Autowired private AnagraficaValidator anagraficaValidator;
 	@Autowired private FileValidator fileValidator;
 	@Autowired private PersonaService personaService;
-	
+
 	public void validate(Object target, Errors errors, String prefix, Set<File> files){
 		LOGGER.info(Utils.getLogMessage("Validazione Persona"));
 		validatePersona(target, errors, prefix);
 		validateFiles(files, errors, "", ((Persona)target).getRuolo());
 		Utils.logDebugErrorFields(LOGGER, errors);
 	}
-	
+
 	private void validatePersona(Object target, Errors errors, String prefix){
 		Persona persona = (Persona)target;
 		validateBase(persona, errors, prefix);
-		
-		
+
+
 		//CELLULARE (LR - DLR - CCS - ComCS - RA)
-		if(	persona.isLegaleRappresentante() || persona.isDelegatoLegaleRappresentante() || 
-			persona.isCoordinatoreComitatoScientifico() || persona.isComponenteComitatoScientifico() || 
+		if(	persona.isLegaleRappresentante() || persona.isDelegatoLegaleRappresentante() ||
+			persona.isCoordinatoreComitatoScientifico() || persona.isComponenteComitatoScientifico() ||
 			persona.isResponsabileAmministrativo()){
 				anagraficaValidator.validateCellulare(persona.getAnagrafica(), errors, prefix + "anagrafica.");
 		}
-		
+
 		//PEC (LR)
 		if(persona.isLegaleRappresentante()){
 			anagraficaValidator.validatePEC(persona.getAnagrafica(), errors, prefix + "anagrafica.");
 		}
-		
+
 		//PROFESSIONE (RSI - RQ - CCS - ComCS)
-		if(persona.isResponsabileSistemaInformatico() || persona.isResponsabileQualita() || 
-			persona.isCoordinatoreComitatoScientifico() || persona.isComponenteComitatoScientifico()){
+		if(persona.isCoordinatoreComitatoScientifico() || persona.isComponenteComitatoScientifico()){
 			if(persona.getProfessione() == null || persona.getProfessione().getNome().isEmpty())
 				errors.rejectValue(prefix + "professione", "error.empty");
 		}
-		
+
 		//COMPONENTE COMITATO SCIENTIFICO
 		boolean cfPresente = false;
 		boolean coordinatorePresente = false;
@@ -61,31 +60,31 @@ public class PersonaValidator {
 			//non e' possibile inserire piu' volte la stessa persona nel comitato
 			Persona personaCF = personaService.getPersonaByRuoloAndAnagraficaId(Ruolo.COMPONENTE_COMITATO_SCIENTIFICO, persona.getAnagrafica().getId(), persona.getProvider().getId());
 			Persona personaCoord = personaService.getCoordinatoreComitatoScientifico(persona.getProvider().getId());
-			
+
 			if(personaCF != null){
 				if(persona.isNew() || !persona.getId().equals(personaCF.getId()))
 					cfPresente = true;
 			}
-			
+
 			if(personaCoord != null){
 				if(persona.isNew() || !persona.getId().equals(personaCoord.getId()))
 					coordinatorePresente = true;
 			}
-			
+
 			if(cfPresente)
 				errors.rejectValue(prefix + "anagrafica.codiceFiscale", "error.componente_presente");
-			
+
 			if(coordinatorePresente && persona.isCoordinatoreComitatoScientifico())
 				errors.rejectValue(prefix + "coordinatoreComitatoScientifico", "error.coordinatore_presente");
 		}
 	}
-	
+
 	private void validateBase(Object target, Errors errors, String prefix){
 		LOGGER.debug("VALIDAZIONE PERSONA");
 		Persona persona = (Persona)target;
 		anagraficaValidator.validateBase(persona.getAnagrafica(), errors, prefix + "anagrafica.", persona.getProvider().getId());
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public void validateFiles(Object target, Errors errors, String prefix, Ruolo ruolo){
 		LOGGER.debug("VALIDAZIONE ALLEGATI");
@@ -94,11 +93,11 @@ public class PersonaValidator {
 			files = (Set<File>) target;
 		else
 			files = new HashSet<File>();
-		
+
 		File attoNomina = null;
 		File cv = null;
 		File delega = null;
-		
+
 		for(File file : files){
 			if(file != null && !file.isNew()){
 				if(file.isATTONOMINA())
@@ -109,17 +108,17 @@ public class PersonaValidator {
 					delega = file;
 			}
 		}
-		
+
 		if(ruolo.equals(Ruolo.DELEGATO_LEGALE_RAPPRESENTANTE)){
 			//l'unico che ha l'atto di delega
 			fileValidator.validate(delega, errors, prefix + "delega");
 		}
-		
+
 		if(!ruolo.equals(Ruolo.DELEGATO_LEGALE_RAPPRESENTANTE)){
 			//solo il DLR NON ha l'atto di nomina
 			fileValidator.validate(attoNomina, errors, prefix + "attoNomina");
 		}
-		
+
 		//tutti hanno il cv
 		fileValidator.validate(cv, errors, prefix + "cv");
 	}
