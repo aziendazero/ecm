44be3fe2388715459cf00dfd452527ae72facb7f
tiommi <tiommi@tiommi-desktop.bo.priv>
2016-08-08 17:47:37 +0200

    aggiunto stato accreditamento nel wrapper generico

--- src/main/java/it/tredi/ecm/web/bean/Wrapper.java
+++ src/main/java/it/tredi/ecm/web/bean/Wrapper.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import it.tredi.ecm.dao.entity.FieldIntegrazioneAccreditamento;
 import it.tredi.ecm.dao.entity.FieldValutazioneAccreditamento;
+import it.tredi.ecm.dao.enumlist.AccreditamentoStatoEnum;
 import it.tredi.ecm.dao.enumlist.IdFieldEnum;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Wrapper {
+	private AccreditamentoStatoEnum statoAccreditamento;
 	private Set<IdFieldEnum> idEditabili = new HashSet<IdFieldEnum>();
 	private Set<FieldIntegrazioneAccreditamento> fieldIntegrazione = new HashSet<FieldIntegrazioneAccreditamento>();
 	private Map<IdFieldEnum, FieldValutazioneAccreditamento> mappa = new HashMap<IdFieldEnum, FieldValutazioneAccreditamento>();
