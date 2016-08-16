61fb25cdb2045e7bb3feaf621fa3f5dbeef45bb4
tiommi <tiommi@tiommi-desktop.bo.priv>
2016-08-16 14:26:47 +0200

    Aggiunta funzione repository, filtra gli account a seconda del profilo

--- src/main/java/it/tredi/ecm/dao/repository/AccountRepository.java
+++ src/main/java/it/tredi/ecm/dao/repository/AccountRepository.java
@@ -6,9 +6,11 @@ import java.util.Set;
 import org.springframework.data.repository.CrudRepository;
 
 import it.tredi.ecm.dao.entity.Account;
+import it.tredi.ecm.dao.entity.Profile;
 
 public interface AccountRepository extends CrudRepository<Account, Long> {
 	Optional<Account> findOneByUsername(String username);
 	Optional<Account> findOneByEmail(String email);
 	Set<Account> findAll();
+	Set<Account> findAllByProfiles(Profile profile);
 }
