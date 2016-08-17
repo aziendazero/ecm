8e5351761c028e02f02926632876e92e0d97d978
tiommi <tiommi@tiommi-desktop.bo.priv>
2016-08-17 17:09:34 +0200

    Implementati service per ottenere la mappa di tutte le valutazioni
    divise per account di ogni oggetto, una con il subset e una con l'id
    dell'oggetto per i multistanza

--- src/main/java/it/tredi/ecm/service/ValutazioneServiceImpl.java
+++ src/main/java/it/tredi/ecm/service/ValutazioneServiceImpl.java
@@ -1,4 +1,7 @@
 package it.tredi.ecm.service;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.transaction.Transactional;
@@ -8,8 +11,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.tredi.ecm.dao.entity.Account;
+import it.tredi.ecm.dao.entity.FieldValutazioneAccreditamento;
 import it.tredi.ecm.dao.entity.Valutazione;
- import it.tredi.ecm.dao.repository.ValutazioneRepository;
+import it.tredi.ecm.dao.enumlist.IdFieldEnum;
+import it.tredi.ecm.dao.enumlist.SubSetFieldEnum;
+import it.tredi.ecm.dao.repository.ValutazioneRepository;
 import it.tredi.ecm.utils.Utils;
 
 @Service
@@ -17,6 +24,7 @@ public class ValutazioneServiceImpl implements ValutazioneService {
 	private static Logger LOGGER = LoggerFactory.getLogger(ValutazioneServiceImpl.class);
 
 	@Autowired private ValutazioneRepository valutazioneRepository;
+	@Autowired private FieldValutazioneAccreditamentoService fieldValutazioneAccreditamentoService;
 
 	@Override
 	public Valutazione getValutazione(Long valutazioneId) {
@@ -46,4 +54,44 @@ public class ValutazioneServiceImpl implements ValutazioneService {
 		return allValutazioni;
 	}
 
+	@Override
+	public Set<Account> getAllValutatoriForAccreditamentoId(Long accreditamentoId) {
+		LOGGER.debug(Utils.getLogMessage("Ottengo la lista dei valutatori dell'accreditamento " + accreditamentoId));
+		Set<Valutazione> valutazioni = valutazioneRepository.findAllByAccreditamentoId(accreditamentoId);
+		Set<Account> valutatori = new HashSet<Account>();
+		for (Valutazione v : valutazioni) {
+			valutatori.add(v.getAccount());
+		}
+		return valutatori;
+	}
+
+	@Override
+	public Map<Account, Map<IdFieldEnum, FieldValutazioneAccreditamento>> getMapValutatoreValutazioniByAccreditamentoIdAndSubSet(Long accreditamentoId, SubSetFieldEnum subset) {
+		LOGGER.debug(Utils.getLogMessage("Genero la mappa valutatori - valutazione dell'accreditamento " + accreditamentoId + " per il subset " + subset));
+		Map<Account, Map<IdFieldEnum, FieldValutazioneAccreditamento>> mappaValutatoreValutazioni = new HashMap<Account, Map<IdFieldEnum, FieldValutazioneAccreditamento>>();
+		Set<Valutazione> allValutazioni = valutazioneRepository.findAllByAccreditamentoId(accreditamentoId);
+		//per ogni valutazione dell'accreditamento
+		for (Valutazione v : allValutazioni) {
+			//mi faccio restituire la valutazione relativa al subset e al determinato valutatore
+			Map<IdFieldEnum, FieldValutazioneAccreditamento> mapValutazioni = fieldValutazioneAccreditamentoService.filterFieldValutazioneBySubSetAsMap(v.getValutazioni(), subset);
+			//inserisco il tutto nella mappa valutatoreValutazioni
+			mappaValutatoreValutazioni.put(v.getAccount(), mapValutazioni);
+		}
+		return mappaValutatoreValutazioni;
+	}
+
+	@Override
+	public Map<Account, Map<IdFieldEnum, FieldValutazioneAccreditamento>> getMapValutatoreValutazioniByAccreditamentoIdAndObjectId(Long accreditamentoId, Long id) {
+		LOGGER.debug(Utils.getLogMessage("Genero la mappa valutatori - valutazione dell'accreditamento " + accreditamentoId + " per l'oggetto " + id));
+		Map<Account, Map<IdFieldEnum, FieldValutazioneAccreditamento>> mappaValutatoreValutazioni = new HashMap<Account, Map<IdFieldEnum, FieldValutazioneAccreditamento>>();
+		Set<Valutazione> allValutazioni = valutazioneRepository.findAllByAccreditamentoId(accreditamentoId);
+		//per ogni valutazione dell'accreditamento
+		for (Valutazione v : allValutazioni) {
+			//mi faccio restituire la valutazione relativa al id dell'oggetto e al determinato valutatore
+			Map<IdFieldEnum, FieldValutazioneAccreditamento> mapValutazioni = fieldValutazioneAccreditamentoService.filterFieldValutazioneByObjectAsMap(v.getValutazioni(), id);
+			//inserisco il tutto nella mappa valutatoreValutazioni
+			mappaValutatoreValutazioni.put(v.getAccount(), mapValutazioni);
+		}
+		return mappaValutatoreValutazioni;
+	}
 }
