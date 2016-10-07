8e64ebf1daab2f8fbf12f7712e0fbb4bb27c8769
tiommi <tiommi@tiommi-desktop.bo.priv>
2016-10-07 11:04:44 +0200

    accorciato nome file

--- src/main/java/it/tredi/ecm/dao/entity/Evento.java
+++ src/main/java/it/tredi/ecm/dao/entity/Evento.java
@@ -44,20 +44,20 @@ import lombok.Setter;
 *  	+ (DataInizioEvento - 4) >= Now() => tutto tranne ANTICIPO dataEvento
 *  	+ (DataInizioEvento - 3) <= Now() => tutto bloccato tranne campi Docenti
 *  	+ (DataInizioEvento) <= Now() => tutto bloccato
-*  
+*
 *  PROVIDER B
 *  	+ (DataInizioEvento - 30) >= Now() => tutto modificabile anche dataInizioEvento (ma limite minimo è Now() + 10) per non corrompere i controlli successivi
 * 		+ (DataInizioEvento - 10) >= Now() => tutto tranne ANTICIPO dataEvento
 *  	+ (DataInizioEvento - 9) <= Now() => tutto bloccato tranne campi Docenti
 *  	+ (DataInizioEvento) <= Now() => tutto bloccato
-*  
+*
 *  RENDICONTAZIONE
 *  	+ (Now() > dataFineEvento && Now() <= dataFineEvento + 90)
 *  	+ Inserimento sponsor effettuato
-*  
+*
 *  INSERIMENTO SPONSOR
 *  	+ (Now() <= dataFineEvento + 90)
-* 
+*
 * */
 public class Evento extends BaseEntity{
 	private static Logger LOGGER = LoggerFactory.getLogger(Evento.class);
@@ -65,11 +65,11 @@ public class Evento extends BaseEntity{
 	 * PREFIX[-edizione]
 	 * 	PREFIX=
 	 * 		*EVENTO inserito in PIANO FORMATIVO -> COD_PROVIDER-ID_EVENTO (quando lo creo nel piano formativo)
-	 * 		
+	 *
 	 * 		*EVENTO inserito in EVENTI a partire da evento in piano formativo -> codiceIdentificativo dell'evento di partenza
-	 * 		
+	 *
 	 * 		*EVENTO inserito da nuovo in EVENTI -> COD_PROVIDER-ID_EVENTO
-	 * 
+	 *
 	 * 		*EVENTO inserito come RIEDIZIONE -> PREFIX dell'evento padre + #edizione
 	 * */
 	private String prefix;
@@ -79,7 +79,7 @@ public class Evento extends BaseEntity{
 			return prefix + "-" + edizione;
 		else return prefix;
 	}
-	
+
 	@Enumerated(EnumType.STRING)
 	private ProceduraFormativa proceduraFormativa;
 	private String titolo;
@@ -104,7 +104,7 @@ public class Evento extends BaseEntity{
 				inverseJoinColumns = @JoinColumn(name = "disciplina_id")
 	)
 	private Set<Disciplina> discipline = new HashSet<Disciplina>();
-	
+
 	public Set<Professione> getProfessioniSelezionate(){
 		Set<Professione> professioniSelezionate = new HashSet<Professione>();
 		if(discipline != null){
@@ -114,31 +114,31 @@ public class Evento extends BaseEntity{
 		return professioniSelezionate;
 	}
 	/**	Fine sezione in comune con EventoPianoFormativo	**/
-	
+
 	/**	Inizio sezione Eventi	**/
 	@OneToOne
 	private Evento eventoPadre;//valorizzato solo se è una riedizione di un evento
 	public boolean isRiedizione(){
 		return eventoPadre != null;
 	}
-	
+
 	//false -> dopo 90gg
 	private boolean canAttachSponsor = true;
 	//true -> dopo fineEvento
 	//false -> dopo aver pagato
 	//false -> se passano i 90 gg e non ha fatto nulla
-	private boolean canDoPagamento = false; 
+	private boolean canDoPagamento = false;
 	//true -> dopo pagamento (Provider B) and attachSponsor fatto
 	//true -> dopo fineEvento (Provider A) and attachSponsor fatto
 	//false -> dopo 90gg
 	private boolean canDoRendicontazione = false;
-	
-	private EventoStatoEnum stato;//vedi descrizione in EventoStatoEnum 
+
+	private EventoStatoEnum stato;//vedi descrizione in EventoStatoEnum
 	private boolean validatorCheck = false; //(durante il salvataggio check di un flag per sapere se sono stati rispettati tutti i vincoli del validator)
-	
+
 	@OneToMany(mappedBy="evento")
 	private Set<RendicontazioneInviata> inviiRendicontazione = new HashSet<RendicontazioneInviata>();
-	
+
 	/**	Utilizzati da Engineering **/
 	private Double costo = 0.00;
 	private Boolean pagato = false;
@@ -149,13 +149,13 @@ public class Evento extends BaseEntity{
 	private File reportPartecipantiXML;
 	@OneToOne
 	private File reportPartecipantiCSV;
-	
+
 	@OneToOne //valorizzato solo se è la realizzazione di un evento descritto nel piano formativo
 	private EventoPianoFormativo eventoPianoFormativo;
 	public boolean isEventoDaPianoFormativo(){
 		return eventoPianoFormativo != null;
 	}
-	
+
 	public void setFromEventoPianoFormativo(EventoPianoFormativo epf){
 		this.setEventoPianoFormativo(epf);
 		this.titolo = epf.getTitolo();
@@ -169,7 +169,7 @@ public class Evento extends BaseEntity{
 		this.prefix = epf.getCodiceIdentificativo();
 		this.edizione = 0;
 	}
-	
+
 	public void buildPrefix(){
 		if(isEventoDaPianoFormativo() && eventoPianoFormativo != null){
 			//evento inserito a partire dall'evento corrispondente in piano formativo
@@ -179,76 +179,76 @@ public class Evento extends BaseEntity{
 			this.prefix = eventoPadre.getPrefix();
 		}
 	}
-	
+
 	@ElementCollection
 	private Set<DestinatariEventoEnum> destinatariEvento = new HashSet<DestinatariEventoEnum>();
 	private ContenutiEventoEnum contenutiEvento;
-	
-	private String provincia; //TODO da lista  
-	private String comune; //TODO da lista 
+
+	private String provincia; //TODO da lista
+	private String comune; //TODO da lista
 	private String indirizzo;//campo libero
 	private String luogo;//campo libero
-	
+
 	@Column(name = "data_inizio")//inizio evento
 	private LocalDate dataInizio;
 	@Column(name = "data_fine")//fine evento
 	private LocalDate dataFine;
-	
+
 	//comprese tra dataInizio e dataFine
 	@ElementCollection
 	private Set<LocalDate> dateIntermedie = new HashSet<LocalDate>();
-	
+
 	private TipologiaEventoRESEnum tipologiaEvento;
 	private boolean workshopSeminariEcm;
 	private String titoloConvegno;
 	private int numeroPartecipanti;
-	
+
 	@OneToMany(mappedBy="eventoResponsabile")
 	private Set<PersonaEvento> responsabili = new HashSet<PersonaEvento>();
 	@OneToMany(mappedBy="eventoDocente")
 	private Set<PersonaEvento> docenti = new HashSet<PersonaEvento>();
-	
+
 	private String programmaAttivitaFormativaRazionale;
 	@ElementCollection
 	private List<String> risultatiAttesi = new ArrayList<String>();
 	@ElementCollection
 	@OrderBy("orario ASC")
 	private List<DettaglioAttivitaRES> programma = new ArrayList<DettaglioAttivitaRES>();
-	
+
 	@OneToOne
 	private File brochureEvento;
-	
+
 	@ElementCollection
 	private Set<VerificaApprendimentoEnum> verificaApprendiemento;
-	
+
 	private float durata;//calcolo automatico
 	private float crediti;//calcolo con algoritmo che puo essere modificato dal provider
 	private boolean confermatiCrediti;
-	
+
 	@OneToOne
 	private PersonaEvento responsabileSegreteriaOrganizzativa;
-	
+
 	private String materialeDurevoleRilasciatoAiPratecipanti;
 	private BigDecimal quotaPartecipazione;
 	private boolean soloLinguaItaliana;
 	private String linguaStranieraUtilizzata;
 	private boolean esisteTraduzioneSimultanea;
-	
+
 	@ElementCollection
 	private Set<VerificaPresenzaPartecipantiEnum> verificaPresenzaPartecipanti;
-	
+
 	private boolean verificaRicaduteFormative;
 	private String descrizioneVerificaRicaduteFormative;
 	@OneToOne
 	private File documentoVerificaRicaduteFormative;
-	
+
 	private boolean eventoSponsorizzato;
 	@OneToMany(mappedBy="evento")
 	private Set<Sponsor> sponsors = new HashSet<Sponsor>();
-	
+
 	private boolean eventoSponsorizzatoDaAziendeAlimentiPrimaInfanzia;
 	@OneToOne
-	private File autocertificazioneAssenzaPartecipazioneAziendeAlimentiPrimaInfanzia;
+	private File autocertificazioneAssenzaAziendeAlimentiPrimaInfanzia;
 	@OneToOne
 	private File autocertificazioneAutorizzazioneMinisteroSalute;
 
@@ -257,18 +257,18 @@ public class Evento extends BaseEntity{
 	private File autocertificazioneAssenzaFinanziamenti;
 	@OneToOne
 	private File contrattiAccordiConvenzioni;
-	
+
 	private boolean eventoAvvalePartner;
 	@OneToMany(mappedBy="evento")
 	private Set<Partner> partners = new HashSet<Partner>();
-	
+
 	@OneToOne
 	private File dichiarazioneAssenzaConflittoInteresse;
-	
+
 	private boolean proceduraVerificaQualitaPercepita;
 
 	private boolean autorizzazionePrivacy;
-	
+
 	public float calcoloDurata(){
 		float durata = 0.0f;
 		for(DettaglioAttivitaRES a : programma){
@@ -276,28 +276,28 @@ public class Evento extends BaseEntity{
 		}
 		return durata;
 	}
-	
+
 	public float calcoloCreditiFormativi(){
 		float crediti = 0.0f;
-		
+
 		if(tipologiaEvento == TipologiaEventoRESEnum.CONVEGNO_CONGRESSO){
 			crediti = (0.20f * durata);
 			if(crediti > 5.0f)
 				crediti = 5.0f;
 		}
-		
+
 		if(tipologiaEvento == TipologiaEventoRESEnum.WORKSHOP_SEMINARIO){
 			crediti = 1 * durata;
 			if(crediti > 50f)
 				crediti = 50f;
 		}
-		
+
 		if(tipologiaEvento == TipologiaEventoRESEnum.CORSO_AGGIORNAMENTO){
 			float creditiFrontale = 0f;
 			float oreFrontale = 0f;
 			float creditiInterattiva = 0f;
 			float oreInterattiva = 0f;
-			
+
 			for(DettaglioAttivitaRES a : programma){
 				if(a.getMetodologiaDidattica().getMetodologia() == TipoMetodologiaEnum.FRONTALE){
 					oreFrontale ++;
@@ -319,17 +319,17 @@ public class Evento extends BaseEntity{
 			}else if(numeroPartecipanti >= 151 && numeroPartecipanti <= 200){
 				creditiFrontale = oreFrontale * 0.5f;
 			}
-			
+
 			//metodologia interattiva
 			creditiInterattiva = oreInterattiva * 1.5f;
-			
+
 			crediti = creditiFrontale + creditiInterattiva;
 		}
-		
+
 		return crediti;
 	}
-	
-	
+
+
 	@Override
     public boolean equals(Object o) {
         if (this == o) {
