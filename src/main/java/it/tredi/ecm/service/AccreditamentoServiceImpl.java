58d16cc806da7831d7fb9881adcc50a5e388306c
Andrea Barducci <abarducci@3di.it>
2016-11-04 17:55:40 +0100

    Salvataggio Data Fine Accreditamento

--- src/main/java/it/tredi/ecm/service/AccreditamentoServiceImpl.java
+++ src/main/java/it/tredi/ecm/service/AccreditamentoServiceImpl.java
@@ -1001,14 +1001,23 @@ public class AccreditamentoServiceImpl implements AccreditamentoService {
 	}
 
 	@Override
-	public void inviaValutazioneCommissione(Long accreditamentoId, CurrentUser curentUser, AccreditamentoStatoEnum stato) throws Exception{
+	public void inviaValutazioneCommissione(Seduta seduta, Long accreditamentoId, CurrentUser curentUser, AccreditamentoStatoEnum stato) throws Exception{
 		workflowService.eseguiTaskTaskInserimentoEsitoOdgForUser(curentUser, getAccreditamento(accreditamentoId), stato);
 		Provider provider = providerService.getProvider(getProviderIdForAccreditamento(accreditamentoId));
-		if(stato == AccreditamentoStatoEnum.ACCREDITATO)
-			provider.setStatus(ProviderStatoEnum.ACCREDITATO_PROVVISORIAMENTE);
+		if(stato == AccreditamentoStatoEnum.ACCREDITATO){
+			Accreditamento accreditamento = getAccreditamento(accreditamentoId);
+			if(accreditamento.isProvvisorio()) {
+				provider.setStatus(ProviderStatoEnum.ACCREDITATO_PROVVISORIAMENTE);
+				accreditamento.setDataFineAccreditamento(seduta.getData().plusYears(4));
+			} else {
+				provider.setStatus(ProviderStatoEnum.ACCREDITATO_STANDARD);
+				accreditamento.setDataFineAccreditamento(seduta.getData().plusYears(2));
+			}
+			save(accreditamento);
+		}
 		if(stato == AccreditamentoStatoEnum.DINIEGO)
 			provider.setStatus(ProviderStatoEnum.DINIEGO);
-		
+		providerService.save(provider);
 		pagamentoService.preparePagamentoProviderPerQuotaAnnua(provider.getId(), LocalDate.now().getYear(), true);
 	}
 
