a5895ceadccfe47689b0358da7de7b51cdf3226c
Andrea Barducci <abarducci@3di.it>
2016-11-10 09:34:35 +0100

    EventoRES copia sede dell'evento in tutti i programmi se non personalizzata nel programma stesso

--- src/main/java/it/tredi/ecm/dao/entity/SedeEvento.java
+++ src/main/java/it/tredi/ecm/dao/entity/SedeEvento.java
@@ -1,9 +1,8 @@
 package it.tredi.ecm.dao.entity;
 
 import java.io.Serializable;
-
+import java.util.Objects;
 import javax.persistence.Embeddable;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,4 +18,61 @@ public class SedeEvento implements Serializable{
 	private String comune; //TODO da lista
 	private String indirizzo;//campo libero
 	private String luogo;//campo libero
+	
+	public SedeEvento() {
+		
+	}
+
+	public SedeEvento(SedeEvento sedeEvento) {
+		if(sedeEvento != null) {
+			this.setComune(sedeEvento.getComune());
+			this.setIndirizzo(sedeEvento.getIndirizzo());
+			this.setLuogo(sedeEvento.getLuogo());
+			this.setProvincia(sedeEvento.getProvincia());
+		}
+	}
+
+	public void copiaDati(SedeEvento sedeEvento) {
+		if(sedeEvento == null) {
+			this.comune = "";
+			this.indirizzo = "";
+			this.luogo = "";
+			this.provincia = "";
+		} else {
+			this.comune = sedeEvento.comune;
+			this.indirizzo = sedeEvento.indirizzo;
+			this.luogo = sedeEvento.luogo;
+			this.provincia = sedeEvento.provincia;
+		}
+	}
+	
+	public boolean isEmpty() {
+		return (comune == null || comune.isEmpty())
+		&& (indirizzo == null || indirizzo.isEmpty())
+		&& (luogo == null || luogo.isEmpty())
+		&& (provincia == null || provincia.isEmpty());
+	}
+
+	public static boolean isEmpty(SedeEvento o) {
+		if(o == null)
+			return true;
+		return o.isEmpty();
+	}
+	
+	public static boolean compare(SedeEvento o1, SedeEvento o2) {
+		if (o1 == o2)
+			return true;
+		//null e empty vengono considerati uguali
+		if (isEmpty(o1) && isEmpty(o2))
+			return true;
+		//a questo punto le sedi != null con l'altra sede null sono sicuramente non empty
+		if ((o1 == null && o2 != null) || o2 == null && o1 != null) 
+			return false;
+		//ora sono entrambe non null
+		return Objects.equals(o1.comune, o2.comune) &&
+		Objects.equals(o1.indirizzo, o2.indirizzo) &&
+		Objects.equals(o1.luogo, o2.luogo) &&
+		Objects.equals(o1.provincia, o2.provincia);
+	}
+	
 }
