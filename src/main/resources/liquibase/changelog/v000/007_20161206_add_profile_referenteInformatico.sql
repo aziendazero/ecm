1ed9069b1e9734f9a0b541d4a8e7790b838d04c4
tiommi <tiommi@tiommi-desktop.bo.priv>
2016-12-07 09:20:44 +0100

    modifiche DB (aggiunto un ruolo + table flux standard e storicizzazione)

---
+++ src/main/resources/liquibase/changelog/v000/007_20161206_add_profile_referenteInformatico.sql
@@ -0,0 +1,21 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = ecmdb, pg_catalog;
+
+
+--
+-- Data for Name: profile; Type: TABLE DATA; Schema: ecmdb; Owner: ecm
+--
+
+INSERT INTO profile (id, profile_enum) VALUES (999, 'REFERENTE_INFORMATICO');
+INSERT INTO profile_role (profile_id, role_id) VALUES (999, 1002);
+INSERT INTO profile_role (profile_id, role_id) VALUES (999, 1006);
