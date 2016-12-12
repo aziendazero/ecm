b096e494e7fa3ce0c8740b0871736c658c6b8fe7
Domenico Pranteda <dpranteda@3di.it>
2016-12-12 09:47:17 +0100

    Oops mi ero dimenticato di fare il changeset liquibase

--- src/main/java/it/tredi/ecm/dao/entity/AlertEmail.java
+++ src/main/java/it/tredi/ecm/dao/entity/AlertEmail.java
@@ -4,7 +4,14 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+
+import org.jvnet.hk2.config.Element;
 
 import it.tredi.ecm.dao.enumlist.AlertTipoEnum;
 import lombok.Getter;
@@ -15,12 +22,18 @@ import lombok.Setter;
 @Entity
 public class AlertEmail extends BaseEntity{
 
+	@Enumerated(EnumType.STRING)
 	private AlertTipoEnum tipo;
+	@Column(name="data_scadenza")
 	private LocalDateTime dataScadenza;
+	@ManyToOne
 	private Provider provider;
+	@ManyToOne
 	private Evento evento;
 
+	@ElementCollection
 	private Set<String> destinatari = new HashSet<String>();
+	@Column(name="data_invio")
 	private LocalDateTime dataInvio;
 	private Boolean inviato;
 }
