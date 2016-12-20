a5e497b6caad2d058a6cc4744daa3185f416fd11
Andrea Barducci <abarducci@3di.it>
2016-12-20 16:15:05 +0100

    Correzione Accreditamento Standard

--- src/main/java/it/tredi/ecm/dao/entity/VerbaleValutazioneSulCampo.java
+++ src/main/java/it/tredi/ecm/dao/entity/VerbaleValutazioneSulCampo.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
