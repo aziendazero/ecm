c7355b0e66ba302cdad580a7a98dbe5fff6020df
tiommi <tiommi@tiommi-desktop.bo.priv>
2016-12-21 12:49:19 +0100

    fix

--- src/main/java/it/tredi/ecm/service/ValutazioneServiceImpl.java
+++ src/main/java/it/tredi/ecm/service/ValutazioneServiceImpl.java
@@ -387,6 +387,7 @@ public class ValutazioneServiceImpl implements ValutazioneService {
 	@Override
 	public void valutaTuttiSi(Accreditamento accreditamento, Account account) {
 		Valutazione valutazione = getValutazioneByAccreditamentoIdAndAccountIdAndNotStoricizzato(accreditamento.getId(), account.getId());
+		valutazione.getValutazioni().clear();
 		valutazione.setValutazioni(fieldValutazioneAccreditamentoService.createAllFieldValutazioneAndSetEsito(true, accreditamento));
 		save(valutazione);
 	}
