21946e0841448b9cfe3a458487dfa4aaec7404f5
tiommi <tiommi@tiommi-desktop.bo.priv>
2017-01-13 14:55:20 +0100

    bugfix modulo14

--- src/main/java/it/tredi/ecm/service/ProviderServiceImpl.java
+++ src/main/java/it/tredi/ecm/service/ProviderServiceImpl.java
@@ -537,7 +537,7 @@ public class ProviderServiceImpl implements ProviderService {
 		if(provider.isCanInsertAccreditamentoStandard())
 			provider.setDataScadenzaInsertAccreditamentoStandard(wrapper.getDataScadenzaInsertDomandaStandard());
 		if(provider.isCanInsertRelazioneAnnuale())
-			provider.setDataScadenzaInsertAccreditamentoStandard(wrapper.getDataScadenzaInsertRelazioneAnnuale());
+			provider.setDataScadenzaInsertRelazioneAnnuale(wrapper.getDataScadenzaInsertRelazioneAnnuale());
 		//status provider
 		provider.setStatus(wrapper.getStato());
 
