428cabfae650f091211dd93da15bad9669385c3e
tiommi <tiommi@tiommi-desktop.bo.priv>
2017-01-27 16:37:10 +0100

    fix

--- src/main/resources/liquibase/changelog/v000/20170127_001_update_obiettiviDiSistema.sql
+++ src/main/resources/liquibase/changelog/v000/20170127_001_update_obiettiviDiSistema.sql
@@ -6,7 +6,7 @@ SET client_min_messages = warning;
 
 SET search_path = ecmdb, pg_catalog;
 
-INSERT INTO obiettivo (id, categoria, codice_cogeaps, nazionale, nome) VALUES (1067, 'DI SISTEMA', '17', true, 'Argomenti di carattere generale: informatica e lingua inglese scientifica di livello avanzato. Normativa in materia sanitaria : i principi etici e civili del SSN (17)');
+INSERT INTO obiettivo (id, categoria, codice_cogeaps, nazionale, nome) VALUES (1067, 'DI_SISTEMA', '17', true, 'Argomenti di carattere generale: informatica e lingua inglese scientifica di livello avanzato. Normativa in materia sanitaria : i principi etici e civili del SSN (17)');
 
 UPDATE evento SET obiettivo_nazionale_id = 1067 WHERE obiettivo_nazionale_id = 1040;
 
