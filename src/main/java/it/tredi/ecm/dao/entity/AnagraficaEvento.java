eeff9ea095935e9aa53deb2afd959e34f931fd7e
Domenico Pranteda <dpranteda@3di.it>
2017-03-02 15:02:17 +0100

    Aggiunto vincolo su db per univocità provider_id,codice_fiscale per le tabelle anagrafica_evento e anagrafica_full_evento

--- src/main/java/it/tredi/ecm/dao/entity/AnagraficaEvento.java
+++ src/main/java/it/tredi/ecm/dao/entity/AnagraficaEvento.java
@@ -3,6 +3,8 @@ package it.tredi.ecm.dao.entity;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -12,6 +14,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = {"codiceFiscale", "provider_id"},
+												name="uk_anagraficaevento_codicefiscale_providerid")})
 public class AnagraficaEvento extends BaseEntity{
 	@Embedded
 	private AnagraficaEventoBase anagrafica;
@@ -19,6 +23,6 @@ public class AnagraficaEvento extends BaseEntity{
 	@JsonIgnore
 	@ManyToOne
 	private Provider provider;
-	
+
 	//TODO procedura che al salvataggio dell'anagrafica va ad aggiornare i campi delle PersonaEvento che sono negli eventi ancora modificabili
 }
--- src/main/java/it/tredi/ecm/dao/entity/AnagraficaFullEvento.java
+++ src/main/java/it/tredi/ecm/dao/entity/AnagraficaFullEvento.java
@@ -3,6 +3,8 @@ package it.tredi.ecm.dao.entity;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -12,13 +14,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = {"codiceFiscale", "provider_id"},
+												name="uk_anagraficafullevento_codicefiscale_providerid")})
 public class AnagraficaFullEvento extends BaseEntity{
 	@Embedded
 	private AnagraficaFullEventoBase anagrafica;
-	
+
 	@JsonIgnore
 	@ManyToOne
 	private Provider provider;
-	
+
 	//TODO procedura che al salvataggio dell'anagrafica va ad aggiornare i campi delle PersonaFullEvento che sono negli eventi ancora modificabili
 }
