2bec23a7eb326f13c7b62e32f86157a2c77d3b72
Domenico Pranteda <dpranteda@3di.it>
2017-03-13 17:25:17 +0100

    bugfix liquibase script - rimosso username workflow da utenti fake comunicazione

--- src/main/resources/liquibase/changelog/v000/20170307_004_aggiunta_account_comunicazioni_segreteria_e_provider.sql
+++ src/main/resources/liquibase/changelog/v000/20170307_004_aggiunta_account_comunicazioni_segreteria_e_provider.sql
@@ -8,11 +8,11 @@ SET search_path = ecmdb, pg_catalog;
 
 -- account fake per comunicazioni segreteria
 INSERT INTO account (id, change_password, codice_fiscale, cognome, data_scadenza_password, email, enabled, expires_date, locked, nome, note, password, username, username_workflow, valutazioni_non_date, provider_id, fake_account_comunicazioni)
-VALUES (nextval('hibernate_sequence'), FALSE, null, 'ECM', null, 'segreteria@ecm.it', TRUE, null, FALSE, 'Segreteria', null, '$2a$10$JCx8DPs0l0VNFotVGkfW/uRyJzFfc8HkTi5FQy0kpHSpq7W4iP69.', 'segreteriacomunicazioni', 'segreteriacomunicazioni', 0, null, TRUE);
+VALUES (nextval('hibernate_sequence'), FALSE, null, 'ECM', null, 'segreteria@ecm.it', TRUE, null, FALSE, 'Segreteria', null, '$2a$10$JCx8DPs0l0VNFotVGkfW/uRyJzFfc8HkTi5FQy0kpHSpq7W4iP69.', 'segreteriacomunicazioni', null, 0, null, TRUE);
 
 -- account fake per comunicazioni provider
 INSERT INTO account (id, change_password, codice_fiscale, cognome, data_scadenza_password, email, enabled, expires_date, locked, nome, note, password, username, username_workflow, valutazioni_non_date, provider_id, fake_account_comunicazioni)
-SELECT nextval('hibernate_sequence'), FALSE, null, 'Provider', null, CONCAT('provider', id, '@comunicazioni.it'), TRUE, null, FALSE, 'Comunicazioni', null, '$2a$10$JCx8DPs0l0VNFotVGkfW/uRyJzFfc8HkTi5FQy0kpHSpq7W4iP69.', CONCAT('provider', id, 'comunicazioni'), CONCAT('provider', id, 'comunicazioni'), 0, id, TRUE
+SELECT nextval('hibernate_sequence'), FALSE, null, 'Provider', null, CONCAT('provider', id, '@comunicazioni.it'), TRUE, null, FALSE, 'Comunicazioni', null, '$2a$10$JCx8DPs0l0VNFotVGkfW/uRyJzFfc8HkTi5FQy0kpHSpq7W4iP69.', CONCAT('provider', id, 'comunicazioni'), null, 0, id, TRUE
 FROM provider;
 
 -- inserimento profilo SEGRETERIA_ACCOUNT_COMUNICAZIONI per account comunicazioni segreteria
