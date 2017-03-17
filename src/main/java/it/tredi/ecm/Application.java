0080880846eb7b64b7248262bb9ea209b9114fcc
Domenico Pranteda <dpranteda@3di.it>
2017-03-17 18:22:13 +0100

    bugfix cas

--- src/main/java/it/tredi/ecm/Application.java
+++ src/main/java/it/tredi/ecm/Application.java
@@ -4,6 +4,7 @@ import javax.servlet.Filter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
@@ -13,20 +14,20 @@ import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 import it.tredi.ecm.config.UserChangePasswordCheckFilter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = VelocityAutoConfiguration.class)
 @EnableScheduling
 @EnableAsync
 public class Application {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 	@Bean
 	public PersistenceAnnotationBeanPostProcessor persistenceBeanPostProcessor() {
 	    return new PersistenceAnnotationBeanPostProcessor();
 	}
-	
+
 	@Bean
     public FilterRegistrationBean myFilter() {
         FilterRegistrationBean registration = new FilterRegistrationBean();
@@ -36,12 +37,12 @@ public class Application {
         registration.setOrder(1);
         return registration;
     }
-	
+
 	@Bean(name = "changePasswordFilter")
 	public Filter changePasswordFilter() {
 	    return new UserChangePasswordCheckFilter();
 	}
-	
+
 	@Bean
     public Java8TimeDialect java8TimeDialect() {
         return new Java8TimeDialect();
