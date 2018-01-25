982166a39cea0f6f46bcc69294324db579c7aa7e
Ledio Papa <lpapa@3dial.eu>
2018-01-25 11:27:52 +0100

    Added Deleted Classes by Mistake

---
+++ src/main/java/it/peng/wr/webservice/protocollo/Corrispondente.java
@@ -0,0 +1,231 @@
+
+package it.peng.wr.webservice.protocollo;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for corrispondente complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="corrispondente">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="nominativo" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="indirizzo" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="cap" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="citta" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="provincia" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="pec" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="fax" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "corrispondente", propOrder = {
+    "nominativo",
+    "indirizzo",
+    "cap",
+    "citta",
+    "provincia",
+    "pec",
+    "fax"
+})
+public class Corrispondente {
+
+    @XmlElementRef(name = "nominativo", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> nominativo;
+    @XmlElementRef(name = "indirizzo", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> indirizzo;
+    @XmlElementRef(name = "cap", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> cap;
+    @XmlElementRef(name = "citta", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> citta;
+    @XmlElementRef(name = "provincia", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> provincia;
+    @XmlElementRef(name = "pec", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> pec;
+    @XmlElementRef(name = "fax", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> fax;
+
+    /**
+     * Gets the value of the nominativo property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getNominativo() {
+        return nominativo;
+    }
+
+    /**
+     * Sets the value of the nominativo property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setNominativo(JAXBElement<String> value) {
+        this.nominativo = value;
+    }
+
+    /**
+     * Gets the value of the indirizzo property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getIndirizzo() {
+        return indirizzo;
+    }
+
+    /**
+     * Sets the value of the indirizzo property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setIndirizzo(JAXBElement<String> value) {
+        this.indirizzo = value;
+    }
+
+    /**
+     * Gets the value of the cap property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getCap() {
+        return cap;
+    }
+
+    /**
+     * Sets the value of the cap property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setCap(JAXBElement<String> value) {
+        this.cap = value;
+    }
+
+    /**
+     * Gets the value of the citta property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getCitta() {
+        return citta;
+    }
+
+    /**
+     * Sets the value of the citta property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setCitta(JAXBElement<String> value) {
+        this.citta = value;
+    }
+
+    /**
+     * Gets the value of the provincia property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getProvincia() {
+        return provincia;
+    }
+
+    /**
+     * Sets the value of the provincia property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setProvincia(JAXBElement<String> value) {
+        this.provincia = value;
+    }
+
+    /**
+     * Gets the value of the pec property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getPec() {
+        return pec;
+    }
+
+    /**
+     * Sets the value of the pec property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setPec(JAXBElement<String> value) {
+        this.pec = value;
+    }
+
+    /**
+     * Gets the value of the fax property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getFax() {
+        return fax;
+    }
+
+    /**
+     * Sets the value of the fax property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setFax(JAXBElement<String> value) {
+        this.fax = value;
+    }
+
+}
