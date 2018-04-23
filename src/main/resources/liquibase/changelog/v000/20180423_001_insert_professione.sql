8a6efb580058b410f2bf4c5fa494bf73bd14d269
Andrea Barducci <abarducci@3di.it>
2018-04-23 14:35:56 +0200

    14008 - Aggiunta “Presenza di partecipanti non ECM” nella tendina campo professioni in evento

---
+++ src/main/resources/liquibase/changelog/v000/20180423_001_insert_professione.sql
@@ -0,0 +1,9 @@
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = ecmdb, pg_catalog;
+
+INSERT INTO professione (id, codice_cogeaps, nome, sanitaria) VALUES (1203, '', 'Presenza di partecipanti non ECM', false);
