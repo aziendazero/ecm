c44a27aa58a83f4f0bdae0b43f1a3d56799a7393
Andrea Barducci <abarducci@3di.it>
2018-05-14 09:59:18 +0200

    Per il task 14008 - Aggiunta “Presenza di partecipanti non ECM” nella tendina campo professioni in evento è stata richiesta la modifica delle etichette di professione e disciplina da “Presenza di partecipanti non ECM” a "Partecipanti senza obbligo ECM"

---
+++ src/main/resources/liquibase/changelog/v000/20180514_002_update_disciplina.sql
@@ -0,0 +1,9 @@
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = ecmdb, pg_catalog;
+
+UPDATE disciplina SET nome='Partecipanti senza obbligo ECM' WHERE id=1172;
