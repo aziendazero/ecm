5d42b16efec4b96b45840bdb177b98ed95a7a4b5
Andrea Barducci <abarducci@3di.it>
2018-09-04 14:36:25 +0200

    ECM 15981 - Inserimento nuova disciplina

---
+++ src/main/resources/liquibase/changelog/v000/20180904_001_insert_disciplina.sql
@@ -0,0 +1,9 @@
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = ecmdb, pg_catalog;
+
+INSERT INTO disciplina (id, codice_cogeaps, nome, professione_id) VALUES (1173, '117', 'Medicina subacquea e iperbarica', 1172);
