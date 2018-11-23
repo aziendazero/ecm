e88e371c80f1b818933b35a024d36139fda7b055
Klaudio Bicaku <kbicaku@3dial.eu>
2018-11-23 16:43:49 +0100

    Cambiamenti per il Ticket#15926 - Modifica data calcolo quote annuali

--- src/main/java/it/tredi/ecm/scheduledtask/PagamentoTask.java
+++ src/main/java/it/tredi/ecm/scheduledtask/PagamentoTask.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,17 @@ import it.tredi.ecm.service.QuotaAnnualeService;
 
 @Component
 public class PagamentoTask {
+	@Value("${pagamento.giornoInizioCalcolaQuotaAnnuale}")
+	private int giornoInizioCalcolaQuotaAnnuale = 01;
+	
+	@Value("${pagamento.messeInizioCalcolaQuotaAnnuale}")
+	private int messeInizioCalcolaQuotaAnnuale = 04;
+	
+	@Value("${pagamento.giornoFinisceCalcolaQuotaAnnuale}")
+	private int giornoFinisceCalcolaQuotaAnnuale = 31;
+	
+	@Value("${pagamento.messeFinisceCalcolaQuotaAnnuale}")
+	private int messeFinisceCalcolaQuotaAnnuale = 12;
 	private static Logger LOGGER = LoggerFactory.getLogger(PagamentoTask.class);
 
 	@Autowired EngineeringService engineeringService;
@@ -28,11 +40,14 @@ public class PagamentoTask {
 
 		engineeringService.esitoPagamentiEventi();
 		engineeringService.esitoPagamentiQuoteAnnuali();
-		int year  = LocalDate.now().getYear(); 
-		LocalDate leftDate1 = LocalDate.of(year, 4, 1);
-		LocalDate rightDate2 = LocalDate.of(year, 12, 31);
+		int year  = LocalDate.now().getYear();
+		LocalDate leftDate1 = LocalDate.of(year, messeInizioCalcolaQuotaAnnuale, giornoInizioCalcolaQuotaAnnuale);
+		LocalDate rightDate2 = LocalDate.of(year, messeFinisceCalcolaQuotaAnnuale, giornoFinisceCalcolaQuotaAnnuale);
 		LocalDate DateNow = LocalDate.now();
-		if(DateNow.getMonthValue() >= leftDate1.getMonthValue() && DateNow.getMonthValue() <= rightDate2.getMonthValue()){
+//		if(DateNow.getMonthValue() >= leftDate1.getMonthValue() && DateNow.getMonthValue() <= rightDate2.getMonthValue()){
+//			quotaAnnualeService.checkAndCreateQuoteAnnualiPerAnnoInCorso();
+//		}
+		if(DateNow.isAfter(leftDate1) || DateNow.isEqual(leftDate1) && (DateNow.isBefore(rightDate2) || DateNow.isEqual(rightDate2))){
 			quotaAnnualeService.checkAndCreateQuoteAnnualiPerAnnoInCorso();
 		}
 		
